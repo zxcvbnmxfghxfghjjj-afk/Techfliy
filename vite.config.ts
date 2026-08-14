1b66384167a3bd07e51862f38b84a4e8d1b98b1e
Shimaa youssef <mwshaywsf@gmail.com>
2026-08-14 14:18:42 +0300

    techh edite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Techfliy/',
+    base: process.env.GITHUB_ACTIONS ? '/Techfliy/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
